0a394785fd0ed78995e229f95499bf06a8d5a0ad
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-01-04 15:30:08 -0300

    feat: create card component and style

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,22 @@
+import { TypePokemon } from "@/pages/index";
+
+import styled from "./index.module.css";
+
+interface InterfacePokemon {
+  pokemonData: TypePokemon;
+}
+
+const Card = ({ pokemonData }: InterfacePokemon) => {
+  const { id, name, image } = pokemonData;
+  return (
+    <div className={styled.Card}>
+      <picture>
+        <img className={styled.img} src={image} />
+      </picture>
+      <p className={styled.titleName} >{name.toUpperCase()}</p>
+      <p className={styled.titleId} >{id}</p>
+    </div>
+  );
+};
+
+export default Card;
