aa693c26a195163392f809769f992235627c8107
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-01-04 15:29:49 -0300

    feat: create index and style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+import Card from "src/components/Card";
+
+import styled from "@/pages/index.module.css";
+
+export type TypePokemon = {
+  id: string,
+  name: string,
+  image: string,
+};
+
+const pokemonList: TypePokemon[] = [
+  {
+    id: '001',
+    name: 'bulbasaur',
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+  },
+];
+
+const Home = () => {
   return (
-    <div>
-      Hello World
+    <div className={styled.background}>
+      <main className={styled.body}>
+        {
+          pokemonList.map((pokemon)=>{
+            return <Card pokemonData={pokemon}/>
+          })
+        }
+      </main>
     </div>
   )
 }
+
+export default Home;
