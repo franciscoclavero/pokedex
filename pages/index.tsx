7b73724b8095fe3dd3765ebc35e9e992e6f7775e
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-01-10 11:09:33 -0300

    refact: new structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,30 @@
-import { useState } from "react";
-import Card from "src/components/Card";
+import { useEffect, useState } from "react";
+import CardList, { TypePokemon } from "src/components/template/CardList";
 import FilterInput from "src/components/FilterInput";
-import InfinityScroll from "src/components/InfinityScroll";
 
 import { consumeApi } from "hooks/consumeAPi";
 import getFilterArray from "../src/components/functions/filterArray";
 
 import styled from "@/pages/index.module.css";
 
-export type TypePokemon = {
-  id: string,
-  name: string,
-  img: string,
-};
-
 const Home = () => {
-  const { data: pokemonList, isFetching } = consumeApi<TypePokemon[]>('master/pokedex.json');
-  const [ filtered, setFiltered ] = useState('');
-  const [renderAmount, setRenderAmount] = useState(14);
+  const { data, isFetching } = consumeApi<TypePokemon[]>('master/pokedex.json');
 
-  const pokemonListFiltered = getFilterArray(pokemonList, filtered);
+  const [ filtered, setFiltered ] = useState<string>('');
+  const [ pokemonList, setPokemonList ] = useState<TypePokemon[]>([]);
+
+  useEffect(() => {
+    setPokemonList(data);
+  }, [data]);
 
   return (
     <div className={styled.background}>
       <main className={styled.body}>
         <FilterInput value={filtered} onChange={(e) => setFiltered(e.target.value)}/>
-        <div className={styled.cardList}>
-        <InfinityScroll
-            endOfListCallback={() => {
-              setRenderAmount((prevValue) => prevValue + 14);
-            }}
-            hasMoreData={pokemonListFiltered.length > renderAmount}
-          >
-            {isFetching && <p>Carregando...</p> }
-            {pokemonListFiltered.map((pokemon, index) => index <= renderAmount && (
-              <Card key={pokemon.id} pokemonData={pokemon}/>
-            ))}
-        </InfinityScroll>
-        </div>
+        <CardList pokemonList={getFilterArray(pokemonList, filtered)} />
       </main>
     </div>
   )
-}
+};
 
 export default Home;
---
+++ src/components/template/CardList/index.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Card from "../Card";
+import InfinityScroll from "src/components/InfinityScroll";
+
+import styled from "./index.module.css";
+
+export type TypePokemon = {
+  id: string,
+  name: string,
+  img: string,
+};
+
+interface InterfaceCardList {
+  pokemonList: TypePokemon[];
+}
+
+const CardList = ( { pokemonList }: InterfaceCardList) => {
+  const [ renderAmount, setRenderAmount ] = useState<number>(14);
+
+  return (
+    <div className={styled.cardList}>
+      <InfinityScroll
+          endOfListCallback={() => {
+            setRenderAmount((prevValue) => prevValue + 15);
+          }}
+          hasMoreData={pokemonList.length > renderAmount}
+        >
+          {pokemonList.map((pokemon, index) => index <= renderAmount && (
+            <Card key={pokemon.id} pokemonData={pokemon}/>
+          ))}
+      </InfinityScroll>
+    </div>
+  );
+};
+
+export default CardList;
